98c907276ce0aa2305dbdb0dfa1530b504403a6e
Angel Velasquez <pcisavel@upc.edu.pe>
2022-05-16 07:43:57 -0500

    feat(Categories): Implemented Property-based relationship with Tutorial.

--- LearningCenter.API/Learning/Domain/Models/Category.cs
+++ LearningCenter.API/Learning/Domain/Models/Category.cs
@@ -4,4 +4,9 @@ public class Category
 {
     public int Id { get; set; }
     public string Name { get; set; }
+    
+    // Relationships
+    
+    public IList<Tutorial> Tutorials { get; set; } = new List<Tutorial>();
+    
 }
--- LearningCenter.API/Learning/Domain/Models/Tutorial.cs
+++ LearningCenter.API/Learning/Domain/Models/Tutorial.cs
@@ -5,4 +5,9 @@ public class Tutorial
     public int Id { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
+    
+    // Relationships
+    public int CategoryId { get; set; }
+    public Category Category { get; set; }
+    
 }
