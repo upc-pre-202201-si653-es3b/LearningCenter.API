ff69182f62538664c2e986998b87a5e2e945ec52
Angel Velasquez <pcisavel@upc.edu.pe>
2022-05-16 07:59:32 -0500

    chore(Response): Created Base Response Class.

---
+++ LearningCenter.API/Shared/Domain/Services/Communication/BaseResponse.cs
@@ -0,0 +1,24 @@
+namespace LearningCenter.API.Shared.Domain.Services.Communication;
+
+public abstract class BaseResponse<T>
+{
+    protected BaseResponse(T resource)
+    {
+        Success = true;
+        Resource = resource;
+        Message = string.Empty;
+        
+    }
+
+
+    protected BaseResponse(string message)
+    {
+        Success = false;
+        Resource = default;
+        Message = message;
+    }
+
+    public bool Success { get; private set; }
+    public string Message { get; private set; }
+    public T Resource { get; private set; }
+}
