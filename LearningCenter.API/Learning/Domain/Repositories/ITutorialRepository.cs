90fc3f4ebfbb1374077ae1bfed74ab816fdb0617
Angel Velasquez <pcisavel@upc.edu.pe>
2022-05-23 08:15:44 -0500

    feat(tutorials): Added Repository contract.

--- LearningCenter.API/Learning/Domain/Repositories/ITutorialRepository.cs
+++ LearningCenter.API/Learning/Domain/Repositories/ITutorialRepository.cs
@@ -1,6 +1,14 @@
+using LearningCenter.API.Learning.Domain.Models;
+
 namespace LearningCenter.API.Learning.Domain.Repositories;
 
-public class ITutorialRepository
+public interface ITutorialRepository
 {
-    
+    Task<IEnumerable<Tutorial>> ListAsync();
+    Task AddAsync(Tutorial tutorial);
+    Task<Tutorial> FindByIdAsync(int id);
+    Task<Tutorial> FindByNameAsync(string name);
+    Task<IEnumerable<Tutorial>> FindByCategoryId(int categoryId);
+    void Update(Tutorial tutorial);
+    void Remove(Tutorial tutorial);
 }
